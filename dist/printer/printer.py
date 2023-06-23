587c481685597750981c07ba35b9cf0099afae4e
answearingmachine <74689366+answearingmachine@users.noreply.github.com>
2023-06-22 21:09:17 -0400

    CONFIG WORKS NOW LOL
    Now reads from an .ini file

--- dist/printer/printer.py
+++
@@ -1,4 +0,0 @@
-from printerApp.addtext import main
-
-if __name__ == 'addtext':
-    main()
--- printerApp/addtext.py
+++ printerApp/addtext.py
@@ -5,18 +5,66 @@ from PIL import ImageFont
 import math
 import pandas as pd
 import sys
+import os
+
 from datetime import date
 
-from printer_config import *
+import configparser
+cfg = configparser.ConfigParser()
+
+#print(os.path.dirname(__file__))
+
+dir_path = dir_path = os.path.dirname(__file__)[:-10].replace("\\","/")
+
+cfg.read(dir_path+"printerconfig.ini")
+
+#print(cfg.sections())
+#print(dir_path)
+
+# kids do NOT ever learn from me
+main_url = cfg.get("sheet.info","url_base")+cfg.get("sheet.info","SHEET_ID")
+cards_url = main_url+cfg.get("sheet.info","url_extend")+cfg.get("sheet.info","cards_tab")
+sigils_url = main_url+cfg.get("sheet.info","url_extend")+cfg.get("sheet.info","sigils_tab")
+info_url = main_url+cfg.get("sheet.info","url_extend")+cfg.get("sheet.info","info_tab")
+
+COLUMN_NAME = cfg.getint("sheet.info","COLUMN_NAME")
+COLUMN_TEMPLE = cfg.getint("sheet.info","COLUMN_TEMPLE")
+COLUMN_TIER = cfg.getint("sheet.info","COLUMN_TIER")
+COLUMN_COST = cfg.getint("sheet.info","COLUMN_COST")
+COLUMN_POWER = cfg.getint("sheet.info","COLUMN_POWER")
+COLUMN_HEALTH = cfg.getint("sheet.info","COLUMN_HEALTH")
+COLUMN_SIGILS = cfg.getint("sheet.info","COLUMN_SIGILS")
+COLUMN_TOKEN = cfg.getint("sheet.info","COLUMN_TOKEN")
+COLUMN_TRAITS = cfg.getint("sheet.info","COLUMN_TRAITS")
+COLUMN_TRIBES = cfg.getint("sheet.info","COLUMN_TRIBES")
+COLUMN_FLAVOR = cfg.getint("sheet.info","COLUMN_FLAVOR")
+COLUMN_CREDIT = cfg.getint("sheet.info","COLUMN_CREDIT")
+
+# meta sigils (cells etc)
+METASIGILS = []
+for i in cfg.get("sigils","METASIGILS").split(","):
+    METASIGILS.append(i)
+#end for
+
+TOKENSIGILS = []
+for i in cfg.get("sigils","TOKENSIGILS").split(","):
+    TOKENSIGILS.append(i)
+#end for
+
+HAPPYGEMS = []
+for i in cfg.get("cost.thresholds","HAPPYGEMS").split(","):
+    HAPPYGEMS.append(i)
+#end for
+
 
 # fonts
-nameFont = ImageFont.truetype('Poly-Regular.ttf', 63)
-statFont = ImageFont.truetype('Cambria.ttf', 109)
-textFont = ImageFont.truetype('Cambria.ttf', 33)
-boldFont = ImageFont.truetype('Cambria-Bold.ttf', 33)
-italFont = ImageFont.truetype('Cambria-Italic.ttf', 33)
-artsFont = ImageFont.truetype('Poly-Regular.ttf', 42)
-formatFont = ImageFont.truetype('Cambria.ttf', 20)
+nameFont = ImageFont.truetype(dir_path+'assets/Poly-Regular.ttf', 63)
+statFont = ImageFont.truetype(dir_path+'assets/fonts/Cambria.ttf', 109)
+textFont = ImageFont.truetype(dir_path+'assets/fonts/Cambria.ttf', 33)
+boldFont = ImageFont.truetype(dir_path+'assets/fonts/Cambria-Bold.ttf', 33)
+italFont = ImageFont.truetype(dir_path+'assets/fonts/Cambria-Italic.ttf', 33)
+artsFont = ImageFont.truetype(dir_path+'assets/fonts/Poly-Regular.ttf', 42)
+formatFont = ImageFont.truetype(dir_path+'assets/fonts/Cambria.ttf', 20)
 
 def fetchSigilText(name):
     sdf = (pd.read_csv(sigils_url)).to_dict('split')
@@ -38,16 +86,13 @@ def fetchSigilText(name):
 # end def
 
 def fetchCardByName(name):
-    # I am a piece of garbage (sung in G blues scale)
-    #print(pd.read_csv(cards_url))
-    
     cdf = pd.read_csv(cards_url).to_dict('split')
     found = False
     n=len(cdf["data"])
     idn = 0
     try:
         for i in range(n):
-            if cdf["data"][i][COLUMNS["name"]].lower()==name.lower():
+            if cdf["data"][i][int(COLUMN_NAME)].lower()==name.lower():
                 idn = i
                 found = True
                 break
@@ -62,6 +107,7 @@ def fetchCardByName(name):
 
 def printAllCards(start=-1,end=99999,mode=0,fmt=""):
     cdf = (pd.read_csv(cards_url)).to_dict('split')
+
     #end = min(len(cdf["data"]),end)
     counter = 0
     if end == 99999 and start != -1:
@@ -72,38 +118,38 @@ def printAllCards(start=-1,end=99999,mode=0,fmt=""):
     # end if
     for index in cardRange:
         card = cdf["data"][index]
-        print("THIS CARD: "+str(card[COLUMNS["name"]]))
+        #print("THIS CARD: "+str(card[COLUMN_NAME]))
         cardInfo = {
-            "name": str(card[COLUMNS["name"]]),
-            "temple": str(card[COLUMNS["temple"]]),
-            "tier": str(card[COLUMNS["tier"]]),
-            "power": card[COLUMNS["power"]],
-            "health": card[COLUMNS["health"]],
-            "token": str(card[COLUMNS["token"]]), # todo lol
-            "flavor": str(card[COLUMNS["flavor"]]),
+            "name": str(card[COLUMN_NAME]),
+            "temple": str(card[COLUMN_TEMPLE]),
+            "tier": str(card[COLUMN_TIER]),
+            "power": card[COLUMN_POWER],
+            "health": card[COLUMN_HEALTH],
+            "token": str(card[COLUMN_TOKEN]), # todo lol
+            "flavor": str(card[COLUMN_FLAVOR]),
 
-            "cost": [],
+            "cost":   [],
             "sigils": [],
             "traits": [],
             "tribes": [],
 
-            "artist": str(card[COLUMNS["illus_credit"]]),
+            "artist": str(card[COLUMN_CREDIT]),
         }
 
         # Power and health
         # Not reading the X value??
         # redesign to ignore it
-        if str(card[COLUMNS["power"]]) == str(math.nan):
+        if str(card[COLUMN_POWER]) == str(math.nan):
             #print("yeah there's no power here")
             cardInfo["power"] = 0
         # end if
-        if str(card[COLUMNS["health"]]) == str(math.nan):
+        if str(card[COLUMN_HEALTH]) == str(math.nan):
             #print("yeah there's no health here")
             cardInfo["health"] = 0
         # end if
 
         # Cost (this will get complicated)
-        rawCostString = str(card[COLUMNS["cost"]]).lower()
+        rawCostString = str(card[COLUMN_COST]).lower()
         #print("cost: "+rawCostString)
         if rawCostString != "free" and not ("nan" in rawCostString):
             typeSplit = rawCostString.split("+")
@@ -128,7 +174,7 @@ def printAllCards(start=-1,end=99999,mode=0,fmt=""):
 
         # Sigils - reworking now
         try:
-            sigilsRaw = card[COLUMNS["sigils"]].split(",")
+            sigilsRaw = card[COLUMN_SIGILS].split(",")
             for i in sigilsRaw:
                 i = i.strip()
                 if i == "":
@@ -150,7 +196,7 @@ def printAllCards(start=-1,end=99999,mode=0,fmt=""):
 
         # Traits
         try:
-            traitsRaw = card[COLUMNS["traits"]].split(",")
+            traitsRaw = card[COLUMN_TRAITS].split(",")
             for i in traitsRaw:
                 i = i.strip()
                 if i == "":
@@ -179,12 +225,12 @@ def printAllCards(start=-1,end=99999,mode=0,fmt=""):
             integer = 1
         # end try
         
-        print(cardInfo)
+        #print(cardInfo)
         if mode==1:
             TTSPath=cardInfo["temple"]+"/"+cardInfo["tier"]+"/"
-            printCard(cardInfo,prefix="output/"+TTSPath,fmt=fmt)
+            printCard(cardInfo,prefix=dir_path+"output/"+TTSPath,fmt=fmt)
         else:
-            printCard(cardInfo,prefix="output/reprint/",fmt=fmt)
+            printCard(cardInfo,prefix=dir_path+"output/reprint/",fmt=fmt)
         counter+=1
     # end for
     print("Done! Printed "+str(counter)+" cards.")
@@ -195,7 +241,7 @@ def printCard(info,savePath="output",show=False,prefix="01x 001 ",fmt=""):
     img = Image.new("RGBA",(112,156))
     
     # The background colors.
-    bgPath = "bg/bg_"
+    bgPath = dir_path+"/assets/bg/bg_"
     if info["tier"]=="Rare" or info["tier"]=="Talking":
         bgPath += "rare_"
     else:
@@ -207,22 +253,22 @@ def printCard(info,savePath="output",show=False,prefix="01x 001 ",fmt=""):
     try:
         bgImg = Image.open(bgPath)
     except FileNotFoundError:
-        bgImg = Image.open("bg/zerror.png")
+        bgImg = Image.open(dir_path+"/assets/bg/zerror.png")
     img.paste(bgImg,(13,27))
     bgImg.close()
 
     # It needs a photo...
     try:
-        artImg = Image.open("art/"+info["name"]+".png")
+        artImg = Image.open(dir_path+"/assets/art/"+info["name"]+".png")
     except FileNotFoundError:
         print("could not find portrait: "+info["name"]+".png")
-        artImg = Image.open("art/nothing lol.png")
+        artImg = Image.open(dir_path+"/assets/art/nothing lol.png")
     # end try
     img.paste(artImg,(13,27),artImg.convert("RGBA"))
     artImg.close()
 
     # And a proper frame to accompany it.
-    framePath = "frames/frame_"
+    framePath = dir_path+"/assets/frames/frame_"
     if info["tier"]=="Talking":
         framePath += "uncommon"
     elif info["tier"]=="Side Deck":
@@ -236,7 +282,7 @@ def printCard(info,savePath="output",show=False,prefix="01x 001 ",fmt=""):
     try:
         frameImg = Image.open(framePath)
     except FileNotFoundError:
-        frameImg = Image.open("frames/zerror.png")
+        frameImg = Image.open(dir_path+"/assets/frames/zerror.png")
     img.paste(frameImg,(0,0),frameImg.convert("RGBA"))
     frameImg.close()
 
@@ -257,21 +303,21 @@ def printCard(info,savePath="output",show=False,prefix="01x 001 ",fmt=""):
         if True: # used to check special cases but no more lol
             string = i[1]
             try:
-                costImg = Image.open("cost/"+string+".png")
+                costImg = Image.open(dir_path+"/assets/cost/"+string+".png")
             except FileNotFoundError:
                 try:
                     string = string[:-1]
-                    costImg = Image.open("cost/"+string+".png")
+                    costImg = Image.open(dir_path+"/assets/cost/"+string+".png")
                 except:
                     print("Unknown cost: "+string)
-                    costImg = Image.open("cost/zerror.png")
+                    costImg = Image.open(dir_path+"/assets/cost/zerror.png")
                 # end try
             # end try
             w = costImg.getbbox()[2]
             h = costImg.getbbox()[3]
 
             try:
-                threshold = COSTTHRESH[string]
+                threshold = cfg.getint("cost.thresholds",string)
             except KeyError:
                 print("didn't find cost limits for "+string)
                 threshold = 2
@@ -294,11 +340,11 @@ def printCard(info,savePath="output",show=False,prefix="01x 001 ",fmt=""):
                           costImg.convert("RGBA")
                           )
                 costx -= ((w-1)+6)
-                alphaPaste(img,costx,costy+1,"cost/x.png")
+                alphaPaste(img,costx,costy+1,dir_path+"assets/cost/x.png")
                 costx-=6
                 for j in range(len(numberString)):
                     chrj = numberString[-j-1]
-                    symbolString = "cost/"+chrj+".png"
+                    symbolString = dir_path+"assets/cost/"+chrj+".png"
                     alphaPaste(img,costx,costy,symbolString)
                     costx -= 6
                 # end for
@@ -370,9 +416,9 @@ def printCard(info,savePath="output",show=False,prefix="01x 001 ",fmt=""):
         #end if
         # fetch icon and paste
         try:
-            sigilImg = Image.open("sigils/"+isig+".png")
+            sigilImg = Image.open(dir_path+"/assets/sigils/"+isig+".png")
         except FileNotFoundError:
-            sigilImg = Image.open("sigils/zerror.png")
+            sigilImg = Image.open(dir_path+"/assets/sigils/zerror.png")
         img.paste(sigilImg,(sigilx,sigily),sigilImg.convert("RGBA"))
 
         # write name
@@ -429,7 +475,7 @@ def printCard(info,savePath="output",show=False,prefix="01x 001 ",fmt=""):
         # TODO: figure out inline italics for tribe names
     # end for
     if info["sigils"] != []:
-        alphaPaste(img,150,sigily-(sigily%10),"Separator_large.png")
+        alphaPaste(img,150,sigily-(sigily%10),dir_path+"/assets/misc/Separator_large.png")
         sigily += 10
     # end if
 
@@ -450,7 +496,7 @@ def printCard(info,savePath="output",show=False,prefix="01x 001 ",fmt=""):
     # end for
     if info["traits"] != []:
         sigily = (math.ceil(sigily/10))*10
-        alphaPaste(img,150,sigily,"Separator_large.png")
+        alphaPaste(img,150,sigily,dir_path+"/assets/misc/Separator_large.png")
         sigily += 10
     # end if
     
@@ -496,9 +542,10 @@ def printCard(info,savePath="output",show=False,prefix="01x 001 ",fmt=""):
 
     # Do not let us forget what this is for in the end.
     if fmt != "":
-        print(fmt)
-        tx,ty = FORMATPOS
-        I1.text(FORMATPOS,fmt,fill=colorSample,font=formatFont,anchor="ma")
+        #print(fmt)
+        tx = cfg.getint("text.format","GAMEFORMAT_X")
+        ty = cfg.getint("text.format","GAMEFORMAT_Y")
+        I1.text((tx,ty),fmt,fill=colorSample,font=formatFont,anchor="ma")
     #end if
         
     # uhhh yeah
@@ -536,13 +583,14 @@ def main():
     #printCard(newInfo) # use this to manually print 1
     # do printAllCards(card=79) to print just the card in row 79
 
-    gameFormatString = FORMAT_NAME +" - "+ str(date.today())
+    gameFormatString = cfg.get("sheet.info","FORMAT_NAME") +" - "+ str(date.today())
 
     while True:
         a = input("print cards\n[start],[end]\n").split(",")       
         n = len(a)
 
         if n>0:
+            # I hate dynamic typing I hate dynamic typing
             if type(a[0]) == type("aa"):
                 s = fetchCardByName(a[0])
             elif type(a[0]) == type(2):
